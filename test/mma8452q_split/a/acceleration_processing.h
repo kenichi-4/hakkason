7bc2d12336f6eba62f8c0f0cd14b47383ca5a90e
kenichi <kenichi427827928@gmail.com>
2026-05-29 22:51:12 +0900

    ファイル構成の整理

--- test/mma8452q_split/a/acceleration_processing.h
+++
@@ -1,8 +0,0 @@
-#ifndef ACCELERATION_PROCESSING_H
-#define ACCELERATION_PROCESSING_H
-
-#include <Arduino.h>
-
-float getMoveAcceleration();
-
-#endif
--- test/mma8452q_split/a/data_acquisition.h
+++
@@ -1,9 +0,0 @@
-#ifndef DATA_ACQUISITION_H
-#define DATA_ACQUISITION_H
-
-#include <Arduino.h>
-
-void setupMMA8452Q();
-void readMMA8452Q(float &x, float &y, float &z);
-
-#endif
